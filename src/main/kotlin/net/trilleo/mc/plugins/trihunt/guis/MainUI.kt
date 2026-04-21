689b4d568a6a1422c52b32d06598e116fb5d4dd2
Trilleo <gongsanle@gmail.com>
2026-04-21 19:09:48 +0800

    Improvement: Add event handling for main UI

--- src/main/kotlin/net/trilleo/mc/plugins/trihunt/guis/MainUI.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/guis/MainUI.kt
@@ -1,19 +1,26 @@
 package net.trilleo.mc.plugins.trihunt.guis
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
 import net.trilleo.mc.plugins.trihunt.registration.PluginGUI
+import net.trilleo.mc.plugins.trihunt.utils.itemStack
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemFlag
 
 class MainUI : PluginGUI(
     id = "main",
-    title = Component.text { "TriHunt Main UI" },
+    title = Component.text { "TriHunt Main UI" }.color(NamedTextColor.GOLD),
     rows = 5
 ) {
     override fun setup(player: Player, inventory: Inventory) {
     }
 
     override fun onClick(event: InventoryClickEvent) {
+        event.isCancelled = true
     }
 }
