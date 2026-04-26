7a690c1a23f67d34c29bc02a65e1698aae050886
Trilleo <gongsanle@gmail.com>
2026-04-26 22:47:42 +0800

    Feature: Add compassItemIdentifier entry to PDCEntryUtil.kt

--- src/main/kotlin/net/trilleo/mc/plugins/trihunt/utils/PDCEntryUtil.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/utils/PDCEntryUtil.kt
@@ -6,11 +6,16 @@ import org.bukkit.plugin.java.JavaPlugin
 class PDCEntryUtil {
     // Namespaced Keys
     class PDCKey(private val plugin: JavaPlugin) {
+        // Key for identifying plugin items
         val itemIdentifierKey = NamespacedKey(plugin, "itemIdentifier")
     }
 
     // Values
     class PDCValue {
+        // itemIdentifier - Main Item
         val mainItemIdentifier = "main-item"
+
+        // itemIdentifier - Hunter Compass
+        val compassItemIdentifier = "hunter-compass"
     }
 }
