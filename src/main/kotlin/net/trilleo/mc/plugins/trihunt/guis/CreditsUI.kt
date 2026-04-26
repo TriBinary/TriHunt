c57f8af443f2ffbf85a8f42c00a0364bb9647486
Trilleo <gongsanle@gmail.com>
2026-04-26 13:35:32 +0800

    Feature: Add buttons and actions to CreditsUI.kt

--- src/main/kotlin/net/trilleo/mc/plugins/trihunt/guis/CreditsUI.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/guis/CreditsUI.kt
@@ -3,16 +3,22 @@ package net.trilleo.mc.plugins.trihunt.guis
 import net.kyori.adventure.key.Key
 import net.kyori.adventure.sound.Sound
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.event.ClickEvent
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
+import net.kyori.adventure.title.Title
 import net.trilleo.mc.plugins.trihunt.enums.FillMode
 import net.trilleo.mc.plugins.trihunt.registration.GUIManager
 import net.trilleo.mc.plugins.trihunt.registration.PluginGUI
 import net.trilleo.mc.plugins.trihunt.utils.itemStack
+import net.trilleo.mc.plugins.trihunt.utils.sendPrefixed
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.meta.SkullMeta
+import java.util.*
 
 class CreditsUI : PluginGUI(
     id = "credits",
@@ -22,19 +28,51 @@ class CreditsUI : PluginGUI(
 ) {
     val slotIndex: Map<String, Int> = mapOf(
         "backButtonSlot" to 36,
-        "closeButtonSlot" to 40
+        "closeButtonSlot" to 40,
+        "authorButtonSlot" to 4,
+        "youtubeButtonSlot" to 21,
+        "githubButtonSlot" to 23
     )
 
+    val youtubeLink = "https://www.youtube.com/@TheTrilleo"
+    val githubLink = "https://github.com/Trilleo"
+
     override fun setup(player: Player, inventory: Inventory) {
+        val authorUUID = "28468a45-b78c-4968-9782-f4f893216066"
+
         val closeButton = itemStack(Material.BARRIER) {
             name("<bold><red>Close")
         }
         val backButton = itemStack(Material.ARROW) {
             name("<bold><gray>Back")
         }
+        val authorButton = itemStack(Material.PLAYER_HEAD) {
+            name("<bold><yellow>Made by <gold>Trilleo")
+            meta {
+                (this as SkullMeta)
+                    .owningPlayer = Bukkit.getPlayer(UUID.fromString(authorUUID))
+            }
+        }
+        val youtubeButton = itemStack(Material.RED_CONCRETE) {
+            name("<bold><red>You<white>Tube")
+            lore(
+                "   ",
+                "<gray>Click for my YouTube link"
+            )
+        }
+        val githubButton = itemStack(Material.GRAY_CONCRETE) {
+            name("<bold><gray>GitHub")
+            lore(
+                "   ",
+                "<gray>Click for my GitHub link"
+            )
+        }
 
         inventory.setItem(slotIndex.getValue("backButtonSlot"), backButton)
         inventory.setItem(slotIndex.getValue("closeButtonSlot"), closeButton)
+        inventory.setItem(slotIndex.getValue("authorButtonSlot"), authorButton)
+        inventory.setItem(slotIndex.getValue("youtubeButtonSlot"), youtubeButton)
+        inventory.setItem(slotIndex.getValue("githubButtonSlot"), githubButton)
     }
 
     override fun onClick(event: InventoryClickEvent) {
@@ -51,6 +89,27 @@ class CreditsUI : PluginGUI(
         }
         if (event.slot == slotIndex.getValue("backButtonSlot")) {
             GUIManager.open(player, "main")
+        }
+        if (event.slot == slotIndex.getValue("authorButtonSlot")) {
+            val title: Title = Title.title(
+                Component.text("Trilleo").color(NamedTextColor.GOLD).decorate(TextDecoration.BOLD),
+                Component.text("PLUGIN AUTHOR").color(NamedTextColor.GRAY),
+                10,
+                40,
+                10
+            )
+
+            player.closeInventory()
+            player.playSound(Sound.sound(Key.key("minecraft:entity.player.levelup"), Sound.Source.PLAYER, 1f, 1f))
+            player.showTitle(title)
+        }
+        if (event.slot == slotIndex.getValue("youtubeButtonSlot")) {
+            player.closeInventory()
+            player.sendPrefixed("<click:open_url:$youtubeLink><gray>[YouTube Link]")
+        }
+        if (event.slot == slotIndex.getValue("githubButtonSlot")) {
+            player.closeInventory()
+            player.sendPrefixed("<click:open_url:$githubLink><gray>[GitHub Link]")
         }
     }
 }
