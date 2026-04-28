fc2049cec81bc1bdb0dfd223a366b5bde95a83e7
Trilleo <gongsanle@gmail.com>
2026-04-28 15:28:31 +0800

    Feature: Add CompassListener.kt to detect compass click

---
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/listeners/interaction/CompassListener.kt
@@ -0,0 +1,49 @@
+package net.trilleo.mc.plugins.trihunt.listeners.interaction
+
+import net.trilleo.mc.plugins.trihunt.data.ServerDataManager
+import net.trilleo.mc.plugins.trihunt.managers.CompassManager
+import net.trilleo.mc.plugins.trihunt.registration.GUIManager
+import net.trilleo.mc.plugins.trihunt.utils.PDCEntryUtil
+import net.trilleo.mc.plugins.trihunt.utils.PDCUtil
+import net.trilleo.mc.plugins.trihunt.utils.sendPrefixed
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.persistence.PersistentDataType
+import org.bukkit.plugin.java.JavaPlugin
+
+class CompassListener(private val plugin: JavaPlugin) : Listener {
+    // Detect clicking
+    @EventHandler
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        val player = event.player
+        val item = event.item
+        if (event.action == Action.RIGHT_CLICK_AIR || event.action == Action.RIGHT_CLICK_BLOCK) {
+            if (item != null && PDCUtil.get(
+                    item,
+                    PDCEntryUtil.PDCKey(plugin).itemIdentifierKey,
+                    PersistentDataType.STRING
+                ) == PDCEntryUtil.PDCValue().compassItemIdentifier
+            ) {
+                event.isCancelled = true
+                CompassManager(plugin).refreshCompass(player, item)
+            }
+        }
+    }
+
+    // Detect main item dropping
+    @EventHandler
+    fun onPlayerDropItem(event: PlayerDropItemEvent) {
+        val item = event.itemDrop.itemStack
+        if (PDCUtil.get(
+                item,
+                PDCEntryUtil.PDCKey(plugin).itemIdentifierKey,
+                PersistentDataType.STRING
+            ) == PDCEntryUtil.PDCValue().compassItemIdentifier
+        ) {
+            event.isCancelled = true
+        }
+    }
+}
