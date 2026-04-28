887a8f394c4bdc521d63077e56810702c0f07502
Trilleo <gongsanle@gmail.com>
2026-04-28 20:49:22 +0800

    Feature: Add death and respawn detection to GameListener.kt

--- src/main/kotlin/net/trilleo/mc/plugins/trihunt/listeners/game/GameListener.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/listeners/game/GameListener.kt
@@ -2,12 +2,19 @@ package net.trilleo.mc.plugins.trihunt.listeners.game
 
 import net.trilleo.mc.plugins.trihunt.data.ServerDataManager
 import net.trilleo.mc.plugins.trihunt.managers.GameManager
+import net.trilleo.mc.plugins.trihunt.managers.ItemManager
+import net.trilleo.mc.plugins.trihunt.utils.PDCEntryUtil
+import net.trilleo.mc.plugins.trihunt.utils.PDCUtil
 import net.trilleo.mc.plugins.trihunt.utils.TeamUtil
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerRespawnEvent
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.java.JavaPlugin
+import kotlin.collections.contains
 
 class GameListener(private val plugin: JavaPlugin) : Listener {
     @EventHandler
@@ -29,5 +36,31 @@ class GameListener(private val plugin: JavaPlugin) : Listener {
                 }
             }
         }
+    }
+
+    @EventHandler
+    fun onDeath(event: PlayerDeathEvent) {
+        val player = event.player
+
+        for (item in event.drops) {
+            if (item != null && PDCUtil.get(
+                    item,
+                    PDCEntryUtil.PDCKey(plugin).itemIdentifierKey,
+                    PersistentDataType.STRING
+                ) in listOf(
+                    PDCEntryUtil.PDCValue().mainItemIdentifier,
+                    PDCEntryUtil.PDCValue().compassItemIdentifier
+                )
+            ) {
+                item.amount = 0
+            }
+        }
+    }
+
+    @EventHandler
+    fun onRespawn(event: PlayerRespawnEvent) {
+        val player = event.player
+
+        GameManager(plugin).updatePluginItem(player)
     }
 }
