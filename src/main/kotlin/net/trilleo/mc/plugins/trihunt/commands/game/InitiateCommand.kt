ad529549e94611c7556d4e54f6babf28ff49cafb
Trilleo <gongsanle@gmail.com>
2026-04-28 14:26:56 +0800

    Improvement: Add output feedback in InitiateCommand.kt

--- src/main/kotlin/net/trilleo/mc/plugins/trihunt/commands/game/InitiateCommand.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/commands/game/InitiateCommand.kt
@@ -3,7 +3,9 @@ package net.trilleo.mc.plugins.trihunt.commands.game
 import net.trilleo.mc.plugins.trihunt.data.ServerDataManager
 import net.trilleo.mc.plugins.trihunt.managers.GameManager
 import net.trilleo.mc.plugins.trihunt.registration.PluginCommand
+import net.trilleo.mc.plugins.trihunt.utils.sendPrefixed
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class InitiateCommand(private val plugin: JavaPlugin) : PluginCommand(
@@ -13,6 +15,12 @@ class InitiateCommand(private val plugin: JavaPlugin) : PluginCommand(
 ) {
     override fun execute(sender: CommandSender, args: Array<out String>): Boolean {
         GameManager(plugin).initiateGame()
+
+        if (sender is Player) {
+            sender.sendPrefixed("<green>Game status set to <bold>inactive")
+        } else {
+            sender.sendMessage("Game status set to inactive")
+        }
 
         return true
     }
