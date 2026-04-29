1f5bcce22687934c4069625b4b8f7dc0f20a37f0
Trilleo <gongsanle@gmail.com>
2026-04-29 13:20:51 +0800

    Backend: Migrate plugin item creation to custom items system

--- src/main/kotlin/net/trilleo/mc/plugins/trihunt/managers/ItemManager.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/managers/ItemManager.kt
@@ -12,38 +12,6 @@ import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.java.JavaPlugin
 
 class ItemManager(private val plugin: JavaPlugin) {
-    fun createMainItem(): ItemStack {
-        val mainItem = itemStack(Material.NETHER_STAR) {
-            name("<bold><gold>TriHunt Menu")
-            lore(
-                "   ",
-                "<gray>[Right Click] to open menu"
-            )
-            enchant(Enchantment.BINDING_CURSE, 1)
-            flag(ItemFlag.HIDE_ENCHANTS)
-            pdc(
-                PDCEntryUtil.PDCKey(plugin).itemIdentifierKey,
-                PersistentDataType.STRING,
-                PDCEntryUtil.PDCValue().mainItemIdentifier
-            )
-        }
-        return mainItem
-    }
-
-    fun createCompassItem(): ItemStack {
-        val compassItem = itemStack(Material.COMPASS) {
-            name("<green>Tracking Compass")
-            enchant(Enchantment.DENSITY, 1)
-            flag(ItemFlag.HIDE_ENCHANTS)
-            pdc(
-                PDCEntryUtil.PDCKey(plugin).itemIdentifierKey,
-                PersistentDataType.STRING,
-                PDCEntryUtil.PDCValue().compassItemIdentifier
-            )
-        }
-        return compassItem
-    }
-
     fun clearPluginItems(player: Player) {
         for (item in player.inventory.contents) {
             if (item != null && PDCUtil.get(
